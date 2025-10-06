a4b6348e9e71c18e9ab658cf05e84526270add93
pilot2254 <109202734+pilot2254@users.noreply.github.com>
2025-10-06 15:28:40 +0200

    uhh

--- ProgramovanieOvce/Assets/Scripts/Sheep.cs
+++ ProgramovanieOvce/Assets/Scripts/Sheep.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class Sheep : MonoBehaviour
 {
-
+	[Header("Main")]
 	[SerializeField]
 	Rigidbody2D rb;
+
+	[Header("Moevement parameters")]
 	[SerializeField]
 	private Vector2 direction;
 	[SerializeField]
 	float speed;
+
+	[Header("Delay Parameters")]
+	[SerializeField]
+	float waitTimeMin;
+	[SerializeField]
+	float waitTimeMax;
+	[SerializeField]
+	float restTimeMin;
+	[SerializeField]
+	float restTimeMax;
+
 
 	Vector2 returnRandomVector()
 	{
@@ -24,10 +37,44 @@ public class Sheep : MonoBehaviour
 		return normalizedDirection;
 	}
 
+	float returnRandomWaitTime()
+	{
+		float randomWaitTime = Random.Range(waitTimeMin, waitTimeMax);
+		randomWaitTime = Mathf.Round(randomWaitTime * 100) / 100; // keep only 2 decimal numbers for randomWaitTime
+
+		return randomWaitTime;
+	}
+
+	float returnRandomRestTime()
+	{
+		float randomRestTime = Random.Range(restTimeMin, restTimeMax);
+		randomRestTime = Mathf.Round(randomRestTime * 100) / 100; // keep only 2 decimal numbers for randomWaitTime
+
+		return randomRestTime;
+	}
+
+	// preco pouzivame coroutines?
+	// Je to preto lebo wait time nemozeme pouzivat v normalnych funkciach
+	// to by potom cely engine "afkoval"
+	IEnumerator MainCorot()
+	{
+		while (true) {
+
+			// variables
+			float randomWaitTime = returnRandomWaitTime();
+			Vector2 normalizedDirection = returnRandomVector();
+
+			Debug.Log("Before wait. Waiting: " + randomWaitTime + " seconds.");
+			yield return new WaitForSeconds(randomWaitTime);
+
+			rb.linearVelocity = normalizedDirection * speed;
+		}
+	}
+
 	void Start()
 	{
-		Vector2 normalizedDirection = returnRandomVector();
 
-		rb.linearVelocity = normalizedDirection * speed;
+		StartCoroutine(MainCorot());
+
 	}
 }
