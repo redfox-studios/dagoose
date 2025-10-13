3ce9164f19f5f4be75535213efbc70a63802127a
pilot2254 <109202734+pilot2254@users.noreply.github.com>
2025-10-13 14:43:58 +0200

    create CameraMovement.cs

--- ProgramovanieOvce/Assets/Scripts/CameraMovement.cs
+++ ProgramovanieOvce/Assets/Scripts/CameraMovement.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+	[SerializeField] private float moveSpeed = 20f;
+	[SerializeField] private float smoothTime = 0.25f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private Vector3 targetPosition;
+	private Vector3 velocity = Vector3.zero;
+
+	private void Start()
+	{
+		targetPosition = transform.position;
+	}
+
+	private void Update()
+	{
+		HandleInput();
+		SmoothMove();
+	}
+
+	private void HandleInput()
+	{
+		float horizontal = Input.GetAxisRaw("Horizontal");
+		float vertical = Input.GetAxisRaw("Vertical");
+
+		Vector3 movement = new Vector3(horizontal, vertical, 0f) * moveSpeed * Time.deltaTime;
+		targetPosition += movement;
+	}
+
+	private void SmoothMove()
+	{
+		transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+	}
 }
